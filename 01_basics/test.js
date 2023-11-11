655f31f74070744967c69b2d63d75cd329090f23
Eshu Dhiman <63957845+EshuDhiman@users.noreply.github.com>
2023-11-11 20:17:12 +0000

    yo

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,9 @@
-console.log("eshu");
+const accountId = 1897 //cant be changed
+let  accountEmail="bkjdabsdj@gmai.com" 
+var accountPassword ="1231"
+//prefer not to use var because of  block scope 
+accountCity="chandiageh"
+accountEmail="changed@" 
+accountPassword="changed1231"
+accountCity="changedcity" 
+console.table([accountId,accountEmail,accountPassword,accountCity])
