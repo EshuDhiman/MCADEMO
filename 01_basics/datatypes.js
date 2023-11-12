c53cd9c050097081f77c2259505ed01399b64999
Eshu Dhiman <63957845+EshuDhiman@users.noreply.github.com>
2023-11-12 05:04:05 +0000

    datatypes

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -1,1 +1,4 @@
 "use strict"; // treat all js code as new versions
+console.log(typeof"eshu");
+console.log(typeof null); // null is a object ?
+console.log(typeof undifined); // undifined ka type he undifined h 
