c5d1cf2ad0321d218e8aae553c639458c9339fda
Eshu Dhiman <63957845+EshuDhiman@users.noreply.github.com>
2023-11-13 12:52:44 +0000

    changes in conversions

--- 01_basics/conversionOperations.js
+++ 01_basics/conversionOperations.js
@@ -1,19 +1,23 @@
-let score = "22"
-console.log(typeof score );
-console.log(typeof (score) ); // can pass as a parameter
-let scoreNumber = Number(score)
-console.log(typeof scoreNumber);
-let score2 = "3321A"
-let val = Number(score2)
-console.log(typeof (score2) ); // can pass as a parameter
-console.log(val); // will give NaN not a number
-let score3 = null
-console.log(typeof score3);
-let val2 = Number(score3)
-console.log(val2); // will give NaN not a number
-console.log(typeof val2);
-// empty string when converted to boolean will give false and when is not empty will give true 
-// number 0 when converted to boolean will give false and any other number will give true 
-a = Boolean(score)
-console.log(a);
-
+// let score = "22"
+// console.log(typeof score );
+// console.log(typeof (score) ); // can pass as a parameter
+// let scoreNumber = Number(score)
+// console.log(typeof scoreNumber);
+// let score2 = "3321A"
+// let val = Number(score2)
+// console.log(typeof (score2) ); // can pass as a parameter
+// console.log(val); // will give NaN not a number
+// let score3 = null
+// console.log(typeof score3);
+// let val2 = Number(score3)
+// console.log(val2); // will give NaN not a number
+// console.log(typeof val2);
+// // empty string when converted to boolean will give false and when is not empty will give true 
+// // number 0 when converted to boolean will give false and any other number will give true 
+// a = Boolean(score)
+// console.log(a);
+// -------------------------------------------------------------------------------
+let val = 3 
+let v = -val
+console.log(3);
+console.table(["1"+2+2,1+2+"2",+true,+""]);
