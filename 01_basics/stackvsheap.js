4ab2a6f1360e3f21b9b585fc2ed659eca111435c
Eshu Dhiman <63957845+EshuDhiman@users.noreply.github.com>
2023-11-13 13:48:09 +0000

    random

--- 01_basics/stackvsheap.js
+++ 01_basics/stackvsheap.js
@@ -11,4 +11,4 @@ let user = {
 console.log("value of user obj", user.userName );
 let user2 = user
 user2.userName = "not eshu" // changes made in user2 will affect the original user becouse objects are stored in heap memomer and are treated as by the pricipal of call by refrence which is nothing but there will be no copies of data and will point to the safe reference of the data
-console.table([user,user2]);
+console.table([user,user2]);;;
