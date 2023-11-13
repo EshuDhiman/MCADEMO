1e2fdeacd9a4f1617ead48929b1f0054afe354d2
Eshu Dhiman <63957845+EshuDhiman@users.noreply.github.com>
2023-11-13 14:12:13 +0000

    strings

---
+++ 01_basics/stringsInJs.js
@@ -0,0 +1,6 @@
+const name = "Eshu"
+const age = 18
+console.log(`name is ${name} and age is ${age}`);
+const game = new String('valorant')
+console.log(game.__proto__);
+console.table([game.length,game.toLocaleUpperCase()]);
